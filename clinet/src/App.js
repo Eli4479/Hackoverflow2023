9efe283e8cca41746c72b5cf119f6c77740a19b6
Eli4479 <patel.aryan.eee21@itbhu.ac.in>
2023-04-13 14:57:13 +0530

    added profile page

---
+++ clinet/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Grid from "./components/Grid";
+import Navbar from "./components/Nav";
+function App() {
+  return (
+    <div className="">
+      <Navbar />
+      <Grid />
+    </div>
+  );
+}
+
+export default App;
